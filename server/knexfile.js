7082525f9d3366ee153370f64a57dfc92b09ece8
eopo <26900313+eopo@users.noreply.github.com>
2024-04-16 18:28:15 +0200

    Refactor knexfile.js

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,65 +1,91 @@
-var nconf = require('nconf');
-var confFile = './config/config.json';
-var logger = require('./log');
-var loglevel = nconf.get('global:loglevel');
+const nconf = require('nconf');
+const logger = require('./log');
 
-if(loglevel = 'debug') {
-  var debugon = true
+const loglevel = nconf.get('global:loglevel');
+
+const debugOn = loglevel === 'debug';
+
+const dbtype = nconf.get('database:type');
+
+// in order to create migration files, client must be hardcoded to 'sqlite3' otherwise it won't work.
+const dbconfig = {
+        client: dbtype,
+        connection: {},
+        useNullAsDefault: true,
+        debug: debugOn,
+        migrations: {
+                tableName: 'knex_migrations',
+                directory: `${__dirname}/knex/migrations`,
+        },
+        seeds: {
+                directory: `${__dirname}/knex/seeds`,
+        },
+        log: {
+                warn(message) {
+                        logger.db.info(JSON.stringify(message));
+                },
+                error(message) {
+                        logger.db.error(JSON.stringify(message));
+                },
+                deprecate(message) {
+                        logger.db.info(JSON.stringify(message));
+                },
+                debug(message) {
+                        logger.db.debug(JSON.stringify(message));
+                },
+        },
+};
+
+if (process.env.NODE_ENV === 'test') {
+        dbconfig.connection = getTestConnection();
 } else {
-  var debugon = false
+        switch (dbtype) {
+                case 'sqlite3':
+                        dbconfig.connection = getSqliteConnection();
+                        break;
+                case 'mysql':
+                        dbconfig.connection = getMySQLConnection();
+                        break;
+                case 'oracledb':
+                        dbconfig.connection = getOracleConnection();
+                        dbconfig.fetchAsString = ['clob'];
+                        break;
+                default:
+                        process.exit('No valid db setting');
+        }
 }
 
-var dbtype = nconf.get('database:type');
-
-//in order to create migration files, client must be hardcoded to 'sqlite3' otherwise it won't work. 
-var dbconfig = {
-    client: dbtype,
-    connection: {},
-    useNullAsDefault: true,
-    debug: debugon,
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: __dirname + '/knex/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/knex/seeds'
-    },
-    log: {
-      warn(message) {
-        logger.db.info(JSON.stringify(message))
-      },
-      error(message) {
-        logger.db.error(JSON.stringify(message))
-      },
-      deprecate(message) {
-        logger.db.info(JSON.stringify(message))
-      },
-      debug(message) {
-        logger.db.debug(JSON.stringify(message))
-      },
-    }
+function getTestConnection() {
+        return {
+                filename: './test/messages.db',
+        };
 }
-if(process.env.NODE_ENV === 'test') {
-  dbconfig.connection.filename = './test/messages.db'
-}else if (dbtype == 'sqlite3') {
-  dbconfig.connection.filename = nconf.get('database:file');
-} else if (dbtype == 'mysql') {
-  dbconfig.connection.host = nconf.get('database:server');
-  dbconfig.connection.port = nconf.get('database:port');
-  dbconfig.connection.user = nconf.get('database:username');
-  dbconfig.connection.password = nconf.get('database:password');
-  dbconfig.connection.database = nconf.get('database:database');
-} else if (dbtype == 'oracledb') {
-  dbconfig.connection.connectString = nconf.get('database:connectString');
-  dbconfig.connection.user = nconf.get('database:username');
-  dbconfig.connection.password = nconf.get('database:password');
-  dbconfig.fetchAsString = ['clob'];
+function getSqliteConnection() {
+        return {
+                filename: nconf.get('database:file'),
+        };
+}
+function getMySQLConnection() {
+        return {
+                host: nconf.get('database:server'),
+                port: nconf.get('database:port'),
+                user: nconf.get('database:username'),
+                password: nconf.get('database:password'),
+                database: nconf.get('database:database'),
+        };
+}
+function getOracleConnection() {
+        return {
+                connectString: nconf.get('database:connectString'),
+                user: nconf.get('database:username'),
+                password: nconf.get('database:password'),
+        };
 }
 
-//this is required because of the silly way knex migrations handle environments 
+// this is required because of the silly way knex migrations handle environments
 module.exports = Object.assign({}, dbconfig, {
-  test: dbconfig,
-  development: dbconfig,
-  staging: dbconfig,
-  production: dbconfig,
+        test: dbconfig,
+        development: dbconfig,
+        staging: dbconfig,
+        production: dbconfig,
 });

db24bf3df9c4ac0b46dea26e79ce6d178239d118
eopo <26900313+eopo@users.noreply.github.com>
2026-03-10 03:28:00 +0100

    chore: remove ingest directory after repo split
    The ingest components have been split into separate repositories:
    - @pagermon/ingest-core: github.com/eopo/pagermon-ingest-core
    - pagermon-ingest-multimon: github.com/eopo/pagermon-ingest-adapter-multimon

--- ingest/lib/message/index.js
+++
@@ -1,9 +0,0 @@
-const { parseLine } = require('./parser');
-const { normalizeFormat, normalizeMessage, validateMessage } = require('./model');
-
-module.exports = {
-  parseLine,
-  normalizeFormat,
-  normalizeMessage,
-  validateMessage,
-};
